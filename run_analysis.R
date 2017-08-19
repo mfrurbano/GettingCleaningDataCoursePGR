04aed4d5cbfc7aef21d3fb0d5f4d6183dbe17e0e
Marcelo Urbano <mfrurbano@gmail.com>
2017-08-19 11:54:08 -0300

    Add files via upload

---
+++ run_analysis.R
@@ -0,0 +1,77 @@
+## Cleaning up
+rm(list=ls())
+
+## Changing working directory to the one where data was extracted 
+setwd("C:/Users/Marcelo/Downloads/UCI HAR Dataset")
+
+## Loading library
+library(data.table)
+library(dplyr)
+
+## Reading main data files
+dtTrain <- read.table("./train/X_train.txt")
+dtTest <- read.table("./test/X_test.txt")
+
+## Reading feature (main files column names)
+feat <- read.table("./features.txt",stringsAsFactors = FALSE)
+
+## Naming main file columns
+names(dtTrain) <- feat$V2
+names(dtTest) <- feat$V2
+
+## Reading subject data and attributing a clear column name
+dtSubjTrain <- read.table("./train/subject_train.txt",stringsAsFactors = FALSE)
+names(dtSubjTrain) <- "subject"
+dtSubjTest <- read.table("./test/subject_test.txt",stringsAsFactors = FALSE)
+names(dtSubjTest) <- "subject"
+
+## Reading activity descriptions and attributing a clear column name
+activity <- read.table("./activity_labels.txt",stringsAsFactors = FALSE)
+names(activity)[2] <- "activityDesc"
+
+## Reading activity code files
+dtTrainLabels <- read.table("./train/y_train.txt",stringsAsFactors = FALSE)
+dtTestLabels <- read.table("./test/y_test.txt",stringsAsFactors = FALSE)
+
+## Binding columns to main files
+##
+## First subject column
+dtTrain <- cbind(dtSubjTrain,dtTrain)
+dtTest <- cbind(dtSubjTest,dtTest)
+
+## Now, activity code
+dtTrain <- cbind(dtTrainLabels,dtTrain)
+dtTest <- cbind(dtTestLabels,dtTest)
+
+## Joining the main files for train and test
+dtJoin <- rbind(dtTrain,dtTest)
+
+## Merging activity codes to the joined file
+##	- joining column was not passed since it has the same name,
+##    - V1, in both files
+dtJoin <- merge(activity,dtJoin)
+
+## Join column V1 was very useful but now can be dropped
+dtJoin <- select(dtJoin, -V1)
+
+## Generating the table with only the global statistics
+##
+## First creating 2 vectos, one for each statistic
+means <- sapply(dtJoin[,3:563],mean)
+sds <- sapply(dtJoin[,3:563],sd)
+## Now joining the 2 and getting the desired table
+measureStats <- data.frame(means,sds)
+## Better names pay off!
+names(measureStats) <- c("mean","std dev")
+
+## Creating a grouped frame for the remaining steps
+dtJoinG <- group_by( dtJoin, .dots = c("subject", "activityDesc"))
+
+## Summarising as requested and getting a tidy data set
+tidy <- summarise_all(dtJoinG, funs(mean))
+
+## Generating the tidy data set file
+write.table(tidy,file="tidy.txt",row.names=FALSE)
+
+
+
